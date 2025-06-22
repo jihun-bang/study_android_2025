e394a39cae3fc30d88c4a6714703d2a7ca00032a
jihun.bang <uwlgnsu@gmail.com>
2025-06-22 20:29:53 +0900

    fix: rename

--- websocet_map/app/src/main/java/com/example/websocet_map/WebSocketMapApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.websocet_map
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WebSocketMapApplication : Application() {
-}
---
+++ websocket_map/app/src/main/java/com/example/websocet_map/WebSocketMapApplication.kt
@@ -0,0 +1,8 @@
+package com.example.websocket_map
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WebSocketMapApplication : Application() {
+}
